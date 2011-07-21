592c5fb61f5dc00de54e533f0ca780039d405f1d
Drew Wells <drew.wells00@gmail.com>
2011-07-21 18:38:53 -0400

    Compresed ttf with FontSquirrel \nAdded all FontSquirrel demos

--- fonttest.js
+++ fonttest.js
@@ -2,7 +2,8 @@ var fonts =  "mplus-1mn-regular.ttf  mplus-2c-regular.ttf " +
     "mplus-1mn-thin.ttf    mplus-2c-thin.ttf " +
     "mplus-1m-regular.ttf  mplus-2m-bold.ttf " +
     "mplus-1c-black.ttf    mplus-1m-thin.ttf      mplus-2m-light.ttf " +
-    "mplus-1c-bold.ttf     mplus-1p-black.ttf     mplus-2m-medium.ttf " +
+    "mplus-1c-bold.ttf     " +
+    "mplus-1p-black.ttf    mplus-2m-medium.ttf " +
     "mplus-1c-heavy.ttf    mplus-1p-bold.ttf      mplus-2m-regular.ttf " +
     "mplus-1c-light.ttf    mplus-1p-heavy.ttf     mplus-2m-thin.ttf " +
     "mplus-1c-medium.ttf   mplus-1p-light.ttf     mplus-2p-black.ttf " +
@@ -15,25 +16,37 @@ var fonts =  "mplus-1mn-regular.ttf  mplus-2c-regular.ttf " +
     "mplus-1mn-light.ttf   mplus-2c-light.ttf " +    
     "mplus-1mn-medium.ttf  mplus-2c-medium.ttf";
 fonts = fonts.match(/\S+/g);
-var l = fonts.length, div, css = document.createElement('style'), name;
+fonts.filter(function( n, i){
+    //console.log( n, i );
+});
+
+var l = fonts.length, div, css = document.createElement('style'), p, name;
 
 while( fonts[--l] ){
 
     name = fonts[l].split('.')[0];
-    div = document.createElement('div');
-    div.setAttribute( 'style', 'font-family: ' + name + ';' );
-    div.innerHTML = fonts[l] + 
-            ': The razor-toothed piranhas of the genera Serrasalmus and Pygocentrus are the most ferocious freshwater fish in the world. In reality, they seldom attack a human.' +
+    p = document.createElement('p');
+    p.setAttribute( 'style', 'font-family: ' + name + ';' );
+    p.innerHTML = 'The razor-toothed piranhas of the genera Serrasalmus and Pygocentrus are the most ferocious freshwater fish in the world. In reality, they seldom attack a human.' +
           '</div>';
+    div = document.createElement('div');
+
+    div.innerHTML = '<label>' + name + '</label> ' +
+        '<a href="' + name + '-webfont.woff">WOFF</a> ' +
+        '<a href="' + name + '-webfont.eot">EOT</a> ' +
+        '<a href="webfonts/' + name.replace('mplus-','M+').replace('-','') + '-demo.html">Demo</a>';
+
+    div.appendChild( p );
 
     document.body.appendChild( div );
 
     css.innerHTML += '\n\n@font-face \n' +
-        '{' + 
-        '  font-family: ' + name + ';\n' +
-        '  src: url("fonts/' + fonts[l] + '")\r\n' +
-        '  format("truetype");' +
-        '}';
+        "{\n" + 
+        "  font-family: " + name + ";\n" +
+        "  src: url('webfonts/" + name + "-webfont.eot');\n" +
+        "  src: url('webfonts/" + name + "-webfont.eot?#iefix') format('embedded-opentype'),\n" +
+        "       url('webfonts/" + name + "-webfont.woff') format('woff')\n" +
+        "}";
     
 }
 document.body.appendChild( css );
